3943c4681d54df2e94b3203257b855499d606a4b
raul <raul@zamacona.me>
2014-10-28 22:19:42 -0600

    done

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,5 @@
 (function($){
-
 	"use strict";
-
 	$(function(){
 
 		/**
@@ -13,21 +11,87 @@
 		};
 
 
-
 		/**
 		 * Regresa todos los valores de un formulario como un associative array
 		 */
 		window.getFormData = function (selector) {
 			var result = [],
 				data   = $(selector).serializeArray();
-
 			$.map(data, function (attr) {
 				result[attr.name] = attr.value;
 			});
 			return result;
 		}
 
+		/**
+		* Fondos para cada sección
+		**/
+		$('.intro').backstretch('images/bg-textura.jpg');
+		$('.que-es-hedbanger').backstretch('images/bg-interior.jpg');
+		$('.barba-barra').backstretch('images/bg-barbero-1.jpg');
+		$('.redes-sociales').backstretch('images/bg-barra.jpg');
+		$('.contacto').backstretch('images/bg-jack.jpg');
 
+
+
+		/**
+		* Navegación
+		**/
+		var contentSections = $('section'),
+		navigationItems = $('#cd-vertical-nav a');
+
+		updateNavigation();
+		$(window).on('scroll', function(){
+			updateNavigation();
+		});
+
+		//smooth scroll to the section
+		navigationItems.on('click', function(event){
+	        event.preventDefault();
+	        smoothScroll($(this.hash));
+	    });
+	    //smooth scroll to second section
+	    $('.scroll-down').on('click', function(event){
+	        event.preventDefault();
+	        smoothScroll($(this.hash));
+	    });
+
+	    //open-close navigation on touch devices
+	    $('.touch .cd-nav-trigger').on('click', function(){
+	    	$('.touch #cd-vertical-nav').toggleClass('open');
+
+	    });
+	    //close navigation on touch devices when selectin an elemnt from the list
+	    $('.touch #cd-vertical-nav a').on('click', function(){
+	    	$('.touch #cd-vertical-nav').removeClass('open');
+	    });
+
+		function updateNavigation() {
+			contentSections.each(function(){
+				var activeSection = $('#cd-vertical-nav a[href="#'+$(this).attr('id')+'"]').data('number') - 1;
+				if ( ( $(this).offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $(this).offset().top + $(this).height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+					if ( activeSection == 0 ){
+						$('h1').css('top', '50%');
+						$('#cd-vertical-nav').css('right', '-400px');
+					} else {
+						$('h1').css('top', '20%');
+						$('#cd-vertical-nav').css('right', '40px');
+					}
+					if ( activeSection == 5 ){
+						$('h1').css('top', '-20%');
+					}
+					navigationItems.eq(activeSection).addClass('is-selected');
+				}else {
+					navigationItems.eq(activeSection).removeClass('is-selected');
+				}
+			});
+		}
+
+		function smoothScroll(target) {
+	        $('body,html').animate(
+	        	{'scrollTop':target.offset().top},
+	        	600
+	        );
+		}
 	});
-
 })(jQuery);
